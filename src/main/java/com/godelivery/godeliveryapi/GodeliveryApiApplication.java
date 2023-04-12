d97acd521e143a895e9902de2d165f90807ab0f1
Thiago Silva <thiagodd.dev@gmail.com>
2023-04-12 17:50:55 -0300

    first commit

---
+++ src/main/java/com/godelivery/godeliveryapi/GodeliveryApiApplication.java
@@ -0,0 +1,13 @@
+package com.godelivery.godeliveryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GodeliveryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GodeliveryApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/godelivery/godeliveryapi/GodeliveryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.godelivery.godeliveryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GodeliveryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
